0862b09b562975f8bd452e29ec0606c436ba484c
eimanalif <eiman.fadlelmowla@gmail.com>
2024-02-23 10:48:52 -0800

    puppet 1

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Puppet Manifest: Creates a file in /tmp with specific permissio
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => "I love Puppet\n",
+}
