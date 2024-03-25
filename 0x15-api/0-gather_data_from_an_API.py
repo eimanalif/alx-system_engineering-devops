230f6dff215cb79f8bcfd0f5bcac38a03dc092c6
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-03-25 18:36:26 +0000

    p

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,7 +7,7 @@ import requests
 import sys
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     # base URL for the JSONPlaceholder API
     url = "https://jsonplaceholder.typicode.com/"
 
